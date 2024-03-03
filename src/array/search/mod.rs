cf40048445870a5adc1824b8bcf12730a9c3e03f
夏夜行船 <MoonDrift@163.com>
2024-03-03 14:37:18 +0800

    feat: find-peak-element

--- src/array/search/mod.rs
+++ src/array/search/mod.rs
@@ -42,3 +42,38 @@ pub fn find_left<T: PartialOrd>(nums: &[T], target: T) -> i32 {
     }
     ans
 }
+
+/**
+ * https://leetcode.cn/problems/find-peak-element/description/
+ * 峰值元素是指其值严格大于左右相邻值的元素。
+ * 给你一个整数数组 nums，找到峰值元素并返回其索引。数组可能包含多个峰值，在这种情况下，返回 任何一个峰值 所在位置即可。
+ * 你可以假设 nums[-1] = nums[n] = -∞ 。
+ * 你必须实现时间复杂度为 O(log n) 的算法来解决此问题。
+ */
+pub fn find_peak_element(nums: Vec<i32>) -> i32 {
+    let n = nums.len();
+    if n == 1 {
+        return 0;
+    }
+    if nums[0] > nums[1] {
+        return 0;
+    }
+    if nums[n - 1] > nums[n - 2] {
+        return (n - 1) as i32;
+    }
+    let mut l: usize = 1;
+    let mut r: usize = n - 2;
+    let mut ans: i32 = -1;
+    while l <= r {
+        let m: usize = l + (r - l) / 2;
+        if nums[m - 1] > nums[m] {
+            r = m - 1;
+        } else if nums[m + 1] > nums[m] {
+            l = m + 1;
+        } else {
+            ans = m as i32;
+            break;
+        }
+    }
+    ans
+}
