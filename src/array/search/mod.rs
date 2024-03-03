63bdd5e1ed2979938dabcbb1cab3e19abea385db
夏夜行船 <MoonDrift@163.com>
2024-03-03 14:00:28 +0800

    feat: find_left

--- src/array/search/mod.rs
+++ src/array/search/mod.rs
@@ -1,3 +1,5 @@
+use std::usize;
+
 /**
  * 最长增长子序列
  */
@@ -16,4 +18,27 @@ pub fn lis<T: PartialOrd>(nums: &[T]) -> usize {
     }
 
     return ans;
+}
+
+/**
+ * 寻找数组中大于等于目标值的最左位置，没有返回-1
+ */
+pub fn find_left<T: PartialOrd>(nums: &[T], target: T) -> i32 {
+    let len: usize = nums.len();
+    if len == 0 {
+        return -1;
+    };
+    let mut l: usize = 0;
+    let mut r: usize = len - 1;
+    let mut ans: i32 = -1;
+    while l <= r {
+        let m = l + (r - l) / 2;
+        if nums[m] >= target {
+            r = m - 1;
+            ans = m as i32;
+        } else {
+            l = m + 1;
+        }
+    }
+    ans
 }
