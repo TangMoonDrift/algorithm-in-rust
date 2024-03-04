adc30a33d62ebf13d1546b2fa30b1160dd650767
夏夜行船 <MoonDrift@163.com>
2024-03-04 20:21:56 +0800

    feat: print_binary

--- src/bit/mod.rs
+++ src/bit/mod.rs
@@ -1,1 +1,16 @@
-
+pub fn print_binary(num: i32) {
+    let mut i = 31;
+    let mut result = String::new();
+    while i >= 0 {
+        match num & (1 << i) {
+            0 => {
+                result += "0";
+            }
+            _ => {
+                result += "1";
+            }
+        }
+        i -= 1
+    }
+    println!("{}", result);
+}
--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,5 @@ use array::search::lis;
 use array::sort::Sort;
 
 fn main() {
-    let mut nums = [0, 5, 7, 1, 9, 4, 6, 2, 8, 3];
-    let len = nums.len();
-    let res = lis(&nums);
-    let mut sortable = Sort::from(&mut nums);
-    sortable
-        .select_sort()
-        .bubble_sort()
-        .insert_sort()
-        .merge_sort()
-        .merge_sort_recursion(0, len - 1);
-    println!("{}", res);
-    println!("{:?}", nums);
-
-    let gcd = algorithm::gcd(12, 8);
-    println!("{}", gcd);
+    bit::print_binary(4);
 }
