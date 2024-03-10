533aca5c9eb4af901c984e724c7dfdce9d238325
夏夜行船 <MoonDrift@163.com>
2024-03-10 20:13:34 +0800

    feat: missing_number

--- src/bit/mod.rs
+++ src/bit/mod.rs
@@ -14,3 +14,19 @@ pub fn print_binary(num: i32) {
     }
     println!("{}", result);
 }
+
+/**
+ * https://leetcode.cn/problems/missing-number/description/
+ * 给定一个包含 [0, n] 中 n 个数的数组 nums ，找出 [0, n] 这个范围内没有出现在数组中的那个数。
+ */
+pub fn missing_number(nums: Vec<i32>) -> i32 {
+    let n = nums.len() as i32;
+    let mut eor_all = 0;
+    let mut eor_has = 0;
+    for i in 0..n {
+        eor_all ^= i;
+        eor_has ^= nums[i as usize];
+    }
+    eor_all ^= n;
+    eor_all ^ eor_has
+}
