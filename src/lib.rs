dbe2b2f9f1dcb0829ef6c54c164026b158367395
夏夜行船 <MoonDrift@163.com>
2024-05-03 15:37:14 +0800

    feat: magical number

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 // 求最大公约数
-pub fn gcd(mut m: usize, mut n: usize) -> usize {
+pub fn gcd(mut m: i32, mut n: i32) -> i32 {
     // assert!(m != 0 && n != 0);
     // if m > n {
     //     return gcd(m - n, n);
@@ -37,13 +37,28 @@ pub fn gcd(mut m: usize, mut n: usize) -> usize {
     }
 }
 
-fn lcm(mut m: usize, mut n: usize) -> usize {
+pub fn lcm(mut m: i32, mut n: i32) -> i32 {
     if m < n {
         m ^= n;
         n ^= m;
         m ^= n;
     }
     m / gcd(m, n) * n
+}
+
+pub fn nth_magical_number(n: i32, a: i32, b: i32) -> i32 {
+    let lcm = lcm(a, b) as i64;
+    let (mut ans, mut l, mut r): (i64, i64, i64) = (0, 0, n as i64 * lcm);
+    while l <= r {
+        let m = (r - l) >> 1 + l;
+        if m / a as i64 + m / b as i64 - m / lcm as i64 >= n as i64 {
+            ans = m;
+            r = m - 1;
+        } else {
+            l = m + 1;
+        }
+    }
+    (ans % 1000_000_007) as i32
 }
 
 #[cfg(test)]
@@ -60,5 +75,11 @@ mod tests {
     fn check_lcm() {
         assert_eq!(lcm(72, 30), 360);
         assert_eq!(lcm(30, 72), 360);
+    }
+
+    #[test]
+    fn check_nth_magical_number() {
+        assert_eq!(nth_magical_number(1, 1, 2), 1);
+        assert_eq!(nth_magical_number(2, 1, 2), 2);
     }
 }
