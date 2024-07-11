38b8a8224f7c3acb2801fd3df0dc5d1866d7fbf6
夏夜行船 <MoonDrift@163.com>
2024-07-11 09:45:34 +0800

    num matrix

--- src/tree/trie.rs
+++ src/tree/trie.rs
@@ -240,6 +240,38 @@ pub fn corp_flight_bookings(bookings: Vec<Vec<i32>>, n: i32) -> Vec<i32> {
     ans[1..=flights_cnt].to_vec()
 }
 
+/**
+ * https://leetcode.cn/problems/range-sum-query-2d-immutable/
+ */
+struct NumMatrix {
+    sum: Vec<Vec<i32>>,
+}
+
+impl NumMatrix {
+    fn new(matrix: Vec<Vec<i32>>) -> Self {
+        let m = matrix.len();
+        let n = matrix[0].len();
+
+        let mut sum = vec![vec![0; n + 1]; m + 1];
+
+        for (i, row) in matrix.iter().enumerate() {
+            for (j, x) in row.iter().enumerate() {
+                sum[i + 1][j + 1] = sum[i + 1][j] + sum[i][j + 1] - sum[i][j] + x;
+            }
+        }
+
+        Self { sum }
+    }
+
+    fn sum_region(&self, row1: i32, col1: i32, row2: i32, col2: i32) -> i32 {
+        let r1 = row1 as usize;
+        let c1 = col1 as usize;
+        let r2 = row2 as usize + 1;
+        let c2 = col2 as usize + 1;
+        self.sum[r2][c2] - self.sum[r2][c1] - self.sum[r1][c2] + self.sum[r1][c1]
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
