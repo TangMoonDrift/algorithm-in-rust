d08a8adbff42c293967da9edaedb3f7cc1e60663
夏夜行船 <MoonDrift@163.com>
2024-07-11 12:03:33 +0800

    feat: update

--- src/tree/trie.rs
+++ src/tree/trie.rs
@@ -272,6 +272,9 @@ impl NumMatrix {
     }
 }
 
+/**
+ * https://leetcode.cn/problems/largest-1-bordered-square/
+ */
 pub fn largest1_bordered_square(grid: Vec<Vec<i32>>) -> i32 {
     let n = grid.len() as i32;
     let m = grid[0].len() as i32;
@@ -332,6 +335,89 @@ pub fn largest1_bordered_square(grid: Vec<Vec<i32>>) -> i32 {
     }
 
     ans * ans
+}
+
+/**
+ * https://leetcode.cn/problems/stamping-the-grid/
+ */
+// 时间复杂度O(n*m)，额外空间复杂度O(n*m)
+pub fn possible_to_stamp(grid: Vec<Vec<i32>>, stamp_height: i32, stamp_width: i32) -> bool {
+    let n = grid.len();
+    let m = grid[0].len();
+    let mut sum = vec![vec![0; m + 1]; n + 1];
+    let mut diff = vec![vec![0; m + 2]; n + 2];
+
+    // sum是前缀和数组
+    // 查询原始矩阵中的某个范围的累加和很快速
+    for i in 0..n {
+        for j in 0..m {
+            sum[i + 1][j + 1] = grid[i][j];
+        }
+    }
+
+    let sum = build(sum);
+
+    // 差分矩阵
+    // 当贴邮票的时候，不再原始矩阵里贴，在差分矩阵里贴
+    // 原始矩阵就用来判断能不能贴邮票，不进行修改
+    // 每贴一张邮票都在差分矩阵里修改
+    let mut a: usize = 1;
+    let mut c: usize = a + stamp_height as usize - 1;
+    while c <= n {
+        let mut b: usize = 1;
+        let mut d: usize = b + stamp_width as usize - 1;
+        while d <= m {
+            // 原始矩阵中 (a,b)左上角点
+            // 根据邮票规格，h、w，算出右下角点(c,d)
+            // 这个区域彻底都是0，那么:
+            // sumRegion(sum, a, b, c, d) == 0
+            // 那么此时这个区域可以贴邮票
+            if sum_region(&sum, a, b, c, d) == 0 {
+                diff = add(diff, a, b, c, d);
+            }
+            b += 1;
+            d += 1;
+        }
+        a += 1;
+        c += 1;
+    }
+
+    let diff = build(diff);
+
+    fn build(mut sum: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        for i in 1..sum.len() {
+            for j in 1..sum[0].len() {
+                sum[i][j] += sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1];
+            }
+        }
+        sum
+    }
+
+    fn sum_region(sum: &Vec<Vec<i32>>, a: usize, b: usize, c: usize, d: usize) -> i32 {
+        sum[c][d] - sum[c][b - 1] - sum[a - 1][d] + sum[a - 1][b - 1]
+    }
+
+    fn add(mut diff: Vec<Vec<i32>>, a: usize, b: usize, c: usize, d: usize) -> Vec<Vec<i32>> {
+        diff[a][b] += 1;
+        diff[c + 1][d + 1] += 1;
+        diff[c + 1][b] -= 1;
+        diff[a][d + 1] -= 1;
+        diff
+    }
+
+    // 检查所有的格子！
+    for i in 0..n {
+        for j in 0..m {
+            // 原始矩阵里：grid[i][j] == 0，说明是个洞
+            // 差分矩阵里：diff[i + 1][j + 1] == 0，说明洞上并没有邮票
+            // 此时返回false
+            if (grid[i][j] == 0 && diff[i + 1][j + 1] == 0) {
+                return false;
+            }
+        }
+    }
+
+    return true;
 }
 
 #[cfg(test)]
