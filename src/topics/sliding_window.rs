409d77d3fb5af573b17b0811b8dd50cc127490ae
夏夜行船 <MoonDrift@163.com>
2024-07-14 16:56:23 +0800

    feat: length_of_longest_substring

--- src/topics/sliding_window.rs
+++ src/topics/sliding_window.rs
@@ -1,5 +1,7 @@
 //! 滑动窗口专题
 
+use std::collections::HashMap;
+
 /**
  * https://leetcode.cn/problems/minimum-size-subarray-sum/description/
  */
@@ -28,9 +30,48 @@ pub fn min_sub_array_len(target: i32, nums: Vec<i32>) -> i32 {
     }
 }
 
-#[test]
-fn test_min_sub_array_len() {
-    assert_eq!(min_sub_array_len(7, vec![2, 3, 1, 2, 4, 3]), 2);
-    assert_eq!(min_sub_array_len(4, vec![1, 4, 4]), 1);
-    assert_eq!(min_sub_array_len(7, vec![2, 3, 1, 2, 4, 3]), 2);
+/**
+ * https://leetcode.cn/problems/longest-substring-without-repeating-characters/description/
+*/
+pub fn length_of_longest_substring(s: String) -> i32 {
+    let n = s.len();
+    if n <= 1 {
+        return n as i32;
+    }
+
+    let mut ans = i32::MIN;
+
+    let mut map = HashMap::<char, usize>::new();
+
+    let (mut l, mut r) = (0, 0);
+
+    while r < n {
+        if let Some(v) = map.get(&s.chars().nth(r).unwrap()) {
+            l = (*v + 1).max(l);
+        }
+        ans = ans.max((r - l + 1) as i32);
+        map.insert(s.chars().nth(r).unwrap(), r);
+        r += 1;
+    }
+
+    ans
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_min_sub_array_len() {
+        assert_eq!(min_sub_array_len(7, vec![2, 3, 1, 2, 4, 3]), 2);
+        assert_eq!(min_sub_array_len(4, vec![1, 4, 4]), 1);
+        assert_eq!(min_sub_array_len(7, vec![2, 3, 1, 2, 4, 3]), 2);
+    }
+
+    #[test]
+    fn test_length_of_longest_substring() {
+        assert_eq!(length_of_longest_substring("abcabcbb".to_string()), 3);
+        assert_eq!(length_of_longest_substring("bbbbb".to_string()), 1);
+        assert_eq!(length_of_longest_substring("pwwkew".to_string()), 3);
+    }
 }
