602fc2e1e219f423fd682796a89cc3d0fc17df97
夏夜行船 <MoonDrift@163.com>
2024-07-31 14:01:31 +0800

    feat: update

--- src/topics/sliding_window.rs
+++ src/topics/sliding_window.rs
@@ -103,4 +103,13 @@ mod tests {
         assert_eq!(length_of_longest_substring("bbbbb".to_string()), 1);
         assert_eq!(length_of_longest_substring("pwwkew".to_string()), 3);
     }
+
+    #[test]
+    fn test_can_complete_circuit() {
+        assert_eq!(
+            can_complete_circuit(vec![1, 2, 3, 4, 5], vec![3, 4, 5, 1, 2]),
+            3
+        );
+        assert_eq!(can_complete_circuit(vec![2, 3, 4], vec![3, 4, 3]), -1);
+    }
 }
