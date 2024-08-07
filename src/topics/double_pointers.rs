d631ce561dded0b6254628cbb229dc0bf3394c7b
夏夜行船 <MoonDrift@163.com>
2024-08-07 17:44:43 +0800

    feat: update

---
+++ src/topics/double_pointers.rs
@@ -0,0 +1,41 @@
+//! 双指针专题
+
+/**
+ * https://leetcode.cn/problems/heaters/
+ */
+pub fn find_radius(houses: Vec<i32>, heaters: Vec<i32>) -> i32 {
+    let n = houses.len();
+    let m = heaters.len();
+    let mut houses = houses.clone();
+    houses.sort();
+    let mut heaters = heaters.clone();
+    heaters.sort();
+
+    let mut l = 0;
+    let mut r = 0;
+    let mut ans = 0;
+
+    fn absolute_value(x: i32) -> i32 {
+        if x < 0 {
+            -x
+        } else {
+            x
+        }
+    }
+
+    fn is_best(houses: &Vec<i32>, heaters: &Vec<i32>, i: usize, j: usize) -> bool {
+        j == heaters.len() - 1
+            || absolute_value(houses[i] - heaters[j]) < absolute_value(houses[i] - heaters[j + 1])
+    }
+
+    while l < n {
+        while !is_best(&houses, &heaters, l, r) {
+            r += 1;
+        }
+        let dis = absolute_value(houses[l] - heaters[r]);
+        ans = ans.max(dis);
+        l += 1;
+    }
+
+    ans
+}
--- src/topics/mod.rs
+++ src/topics/mod.rs
@@ -1,3 +1,4 @@
 pub mod diff;
+pub mod double_pointers;
 pub mod greedy;
 pub mod sliding_window;
