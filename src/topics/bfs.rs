e536dc6cd5d3488ff0ad594e5c7dd0f8c7684db5
夏夜行船 <MoonDrift@163.com>
2024-08-23 15:08:49 +0800

    feat: leetcode 1162. 地图分析

--- src/topics/bfs.rs
+++ src/topics/bfs.rs
@@ -1,5 +1,7 @@
 //? 宽度优先遍历专题
 
+use std::collections::VecDeque;
+
 /**
  * 1162. 地图分析
  * https://leetcode.cn/problems/as-far-from-land-as-possible/
@@ -58,4 +60,56 @@ pub fn max_distance(grid: Vec<Vec<i32>>) -> i32 {
     }
 
     level - 1
+}
+
+/**
+ * 1368. 使网格图至少有一条有效路径的最小代价
+ * https://leetcode.cn/problems/minimum-cost-to-make-at-least-one-valid-path-in-a-grid/
+ */
+pub fn min_cost(grid: Vec<Vec<i32>>) -> i32 {
+    const MOVE: [[i32; 2]; 5] = [[0, 0], [0, 1], [0, -1], [1, 0], [-1, 0]];
+    let n = grid.len();
+    let m = grid[0].len();
+    let mut distance = vec![vec![i32::MAX; m]; n];
+
+    let mut deque = VecDeque::<[i32; 2]>::new();
+    distance[0][0] = 0;
+    deque.push_front([0, 0]);
+
+    while !deque.is_empty() {
+        let record = deque.pop_front().unwrap();
+        let x = record[0];
+        let y = record[1];
+
+        if x == (n - 1) as i32 && y == (m - 1) as i32 {
+            return distance[x as usize][y as usize];
+        }
+
+        for i in 1..=4 {
+            let next_x = x + MOVE[i][0];
+            let next_y = y + MOVE[i][1];
+            let weight = if grid[x as usize][y as usize] == i as i32 {
+                0
+            } else {
+                1
+            };
+            if next_x >= 0
+                && next_x < n as i32
+                && next_y >= 0
+                && next_y < m as i32
+                && distance[x as usize][y as usize] + weight
+                    < distance[next_x as usize][next_y as usize]
+            {
+                distance[next_x as usize][next_y as usize] =
+                    distance[x as usize][y as usize] + weight;
+                if weight == 0 {
+                    deque.push_front([next_x, next_y]);
+                } else {
+                    deque.push_back([next_x, next_y]);
+                }
+            }
+        }
+    }
+
+    -1
 }
