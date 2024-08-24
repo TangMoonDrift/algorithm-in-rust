461cfd6a143303cc63d911c7f2fd958cca52d990
夏夜行船 <MoonDrift@163.com>
2024-08-24 17:04:43 +0800

    feat: leetcode 64. 最小路径和

--- src/topics/dp/dim2.rs
+++ src/topics/dp/dim2.rs
@@ -1,1 +1,29 @@
+//? 二维动态规划专题
 
+/**
+ * 64. 最小路径和
+ * https://leetcode.cn/problems/minimum-path-sum/description/
+ */
+pub fn min_path_sum(grid: Vec<Vec<i32>>) -> i32 {
+    let m = grid.len();
+    let n = grid[0].len();
+    let mut dp = vec![0; n];
+    dp[0] = grid[0][0];
+
+    for i in 1..n {
+        dp[i] = dp[i - 1] + grid[0][i];
+    }
+
+    for i in 1..m {
+        dp[0] += grid[i][0];
+        for j in 1..n {
+            dp[j] = if dp[j - 1] < dp[j] {
+                dp[j - 1] + grid[i][j]
+            } else {
+                dp[j] + grid[i][j]
+            }
+        }
+    }
+
+    dp[n - 1]
+}
