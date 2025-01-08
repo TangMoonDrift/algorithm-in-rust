3fb1e41fa19d1b2514cb7e281246dc04222bc68d
夏夜行船 <MoonDrift@163.com>
2025-01-08 16:13:23 +0800

    feat: min_insertions

--- src/topics/dp/interval.rs
+++ src/topics/dp/interval.rs
@@ -1,6 +1,39 @@
 //? 区间DP专题
 
 /**
+ * 1312. 让字符串成为回文串的最少插入次数
+ * https://leetcode.cn/problems/minimum-insertion-steps-to-make-a-string-palindrome/description/
+ */
+pub fn min_insertions(s: String) -> i32 {
+    let bytes = s.as_bytes();
+    let n = bytes.len();
+    if n == 1 {
+        return 0;
+    } else if n == 2 {
+        return if bytes[0] == bytes[1] { 0 } else { 1 };
+    }
+
+    let mut dp = vec![0; n];
+    dp[n - 1] = if bytes[n - 1] == bytes[n - 2] { 0 } else { 1 };
+
+    for l in (0..(n - 2)).rev() {
+        let mut left_down = dp[l + 1];
+        dp[l + 1] = if bytes[l] == bytes[l + 1] { 0 } else { 1 };
+        for r in (l + 2)..n {
+            let backup = dp[r];
+            if bytes[l] == bytes[r] {
+                dp[r] = left_down;
+            } else {
+                dp[r] = dp[r].min(dp[r - 1]) + 1;
+            }
+            left_down = backup;
+        }
+    }
+
+    dp[n - 1]
+}
+
+/**
  * 486. 预测赢家
  * https://leetcode.cn/problems/predict-the-winner/description/
  */
