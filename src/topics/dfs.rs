a1dbb10f72ad96d064f2356603a5cb6d7e091fbc
夏夜行船 <MoonDrift@163.com>
2025-02-17 14:17:49 +0800

    feat: solve

--- src/topics/dfs.rs
+++ src/topics/dfs.rs
@@ -36,3 +36,59 @@ pub fn num_islands(mut grid: Vec<Vec<char>>) -> i32 {
 
     ans
 }
+
+/**
+ * 130. 被围绕的区域
+ * https://leetcode.cn/problems/surrounded-regions/description/
+ */
+pub fn solve(board: &mut Vec<Vec<char>>) {
+    let n = board.len();
+    if n == 0 {
+        return;
+    }
+    let m = board[0].len();
+
+    for i in 0..m {
+        if board[0][i] == 'O' {
+            dfs(board, 0 as i32, i as i32, n, m);
+        }
+        if board[n - 1][i] == 'O' {
+            dfs(board, (n - 1) as i32, i as i32, n, m);
+        }
+    }
+    for j in 1..(n - 1) {
+        if board[j][0] == 'O' {
+            dfs(board, j as i32, 0 as i32, n, m);
+        }
+        if board[j][m - 1] == 'O' {
+            dfs(board, j as i32, (m - 1) as i32, n, m);
+        }
+    }
+
+    for i in 0..n {
+        for j in 0..m {
+            if board[i][j] == 'O' {
+                board[i][j] = 'X'; // 修改为赋值操作
+            }
+            if board[i][j] == 'F' {
+                board[i][j] = 'O'; // 修改为赋值操作
+            }
+        }
+    }
+
+    fn dfs(board: &mut Vec<Vec<char>>, i: i32, j: i32, n: usize, m: usize) {
+        if i < 0
+            || (i as usize) == n
+            || j < 0
+            || (j as usize) == m
+            || board[i as usize][j as usize] != 'O'
+        {
+            return;
+        }
+        board[i as usize][j as usize] = 'F';
+        dfs(board, i - 1, j, n, m);
+        dfs(board, i + 1, j, n, m);
+        dfs(board, i, j - 1, n, m);
+        dfs(board, i, j + 1, n, m);
+    }
+}
